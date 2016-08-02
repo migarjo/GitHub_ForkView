433f0f425929340bca25f05b3f169b167a1ab72a
Michael Johnson <mjohn174@gmail.com>
2016-08-02 17:45:38 -0600

    Add more handling for Forks
    I added a couple of methods to get the specified repo, then to list the top 30 starred forks with the number of stars and forks each of those have. 
    
    I also renamed the variables to fit with proper PowerShell styling convention.

--- GitHub_DataView.ps1
+++ GitHub_DataView.ps1
@@ -1,6 +1,22 @@
 #Initial Variables
-$username='AHAAAAAAA'
-$repo='PokemonGo-Map'
+$UserName='AHAAAAAAA'
+$RepoName='PokemonGo-Map'
 
-$stargazers=Invoke-RestMethod -uri "https://api.github.com/repos/$username/$repo/stargazers"
-$stargazers[0]
+$Repo=Invoke-RestMethod -uri "https://api.github.com/repos/$UserName/$RepoName"
+#$Repo
+
+$Parameters = @{
+  sort = 'stargazers'
+}
+
+$ForkArray=Invoke-RestMethod -uri $Repo.forks_url -Body $Parameters
+#$ForkArray
+
+
+
+ foreach ($Fork in $ForkArray) {
+   "User " + $Fork.owner.login + "'s fork has " + $Fork.stargazers_count + " stars and " + $Fork.forks_count+ " forks."
+ }
+
+#$StargazerArray=Invoke-RestMethod -uri "https://api.github.com/repos/$UserName/$RepoName/stargazers"
+#$StargazerArray[0]
