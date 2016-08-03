597f750a7f379ba62a10f44bfc472a2bb9240a49
Michael Johnson <mjohn174@gmail.com>
2016-08-03 18:51:06 -0500

    Fold Fork Array call into Function
    I just moved the fork array into a method, since I anticipate reusing it
    fairly often.

--- GitHub_DataView.ps1
+++ GitHub_DataView.ps1
@@ -2,21 +2,32 @@
 $UserName='AHAAAAAAA'
 $RepoName='PokemonGo-Map'
 
-$Repo=Invoke-RestMethod -uri "https://api.github.com/repos/$UserName/$RepoName"
-#$Repo
 
-$Parameters = @{
-  sort = 'stargazers'
+$Repo=Invoke-RestMethod -uri "https://api.github.com/repos/$UserName/$RepoName"
+$Repo
+
+Function GetForks{
+
+	$value = @{}
+
+	$value.Parameters = @{
+	  sort = 'stargazers'
+	}
+
+	$value.ForkArray=Invoke-RestMethod -uri $Repo.forks_url -Body $value.Parameters
+	
+	return $value
 }
 
-$ForkArray=Invoke-RestMethod -uri $Repo.forks_url -Body $Parameters
-#$ForkArray
+$ForkArray=$(GetForks).ForkArray
+
+foreach ($Fork in $ForkArray) {
+	Write-Host "User " $Fork.owner.login"'s fork has "$Fork.stargazers_count" stars and "$Fork.forks_count" forks."
+}
 
 
 
- foreach ($Fork in $ForkArray) {
-   "User " + $Fork.owner.login + "'s fork has " + $Fork.stargazers_count + " stars and " + $Fork.forks_count+ " forks."
- }
+
 
 #$StargazerArray=Invoke-RestMethod -uri "https://api.github.com/repos/$UserName/$RepoName/stargazers"
 #$StargazerArray[0]
