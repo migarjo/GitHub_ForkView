95d12bdfda3996a6e46024dae84a36245c5a3e33
Michael Johnson <mjohn174@gmail.com>
2016-08-04 01:48:30 -0500

    Add Navigation between Forked Repos
    I added a few prompts to allow a user to select a fork that they want to
    make the focus. This also builds a hash table that builds a relationship
    with the selected fork and its "Parent" repository. That way, if a user
    wants to trace back up to parent repositories, they can as long as they
    have been there before in this session. I would think the parent repo is
    accessible somewhere I haven't seen in the API, so a future enhancement
    may render this unnecessary.
    
    TODO:
    
    Add option to open the focused repo in a web browser
    
    Write in some validation for user input - right now a user can break
    this very easily
    
    Allow the Full Path of the first repository to be passed into the script
    as a method
    
    Update Documentation in README.md

--- GitHub_DataView.ps1
+++ GitHub_DataView.ps1
@@ -1,22 +1,25 @@
 #Initial Variables
-$UserName='AHAAAAAAA'
-$RepoName='PokemonGo-Map'
+$baseReposURL='https://api.github.com/repos'
+$fullName='AHAAAAAAA/PokemonGo-Map'
 
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}" -f $Env:GITHUB_OAUTH_TOKEN)))
-$Headers=@{Authorization=("Basic {0}" -f $base64AuthInfo)}
+$headers=@{Authorization=("Basic {0}" -f $base64AuthInfo)}
+$parentUserRepo=@{}
 
+
+#Functions
 Function GetRepo{
 	$value = @{}
-	$value.Repo=Invoke-RestMethod -headers $Headers -uri "https://api.github.com/repos/$UserName/$RepoName"
+	$value.Repo=Invoke-RestMethod -headers $Headers -uri $repoURL
 
 	return $value
 }
 
 Function DisplayRepo{
-	Write-Host "`n`n`nRepository: "$Repo.full_Name"`nDescription: "$Repo.description"`nStars: "$Repo.stargazers_count"`nForks: "$Repo.forks_count
+	Write-Host "`n`n`nRepository: "$repo.full_Name"`nDescription: "$repo.description"`nStars: "$repo.stargazers_count"`nForks: "$repo.forks_count
+	if ($parentUserRepo.ContainsKey($repo.url)){
+	Write-Host "Forked from: "$parentUserRepo.get_item("repo.url")}
 }
-
-
 
 Function GetForks{
 
@@ -26,25 +29,58 @@ Function GetForks{
 	  sort = 'stargazers'
 	}
 
-	$value.ForkArray=Invoke-RestMethod -headers $Headers -uri $Repo.forks_url -Body $value.Parameters
+	$value.ForkArray=Invoke-RestMethod -headers $headers -uri $repo.forks_url -Body $value.Parameters
 
 	return $value
 }
 
 Function DisplayForkTable{
 	Write-Host "`n`nNumber`t`tUser`t`t`tStars`t`tForks`t`t"
-	
-	$ForkIncr = 1
+
 	for ($i=1;$i -lt 11;$i++) {
-		Write-Host "$i`t`t"$ForkArray[$i].owner.login"`t`t"$ForkArray[$i].stargazers_count"`t`t"$ForkArray[$i].forks_count
+		Write-Host "$i`t`t"$forkArray[$i-1].owner.login"`t`t"$forkArray[$i-1].stargazers_count"`t`t"$forkArray[$i-1].forks_count
 	}
 }
 
+Function GetParentUserRepoURL{
+	$value = @{}
+	if ($parentUserRepo.containsKey($repo.url)){
+		$value.URL = $parentUserRepo.get_item($repo.url)
+	}
+	else{
+		Write-Host "Error: Cannot find a repository from which this has been forked"
+		$value.URL = $repo.url
+	}
+	
+	return $value
+}
 
-$Repo=$(GetRepo).Repo
-DisplayRepo
-$ForkArray=$(GetForks).ForkArray
-DisplayForkTable
+$repoURL = "" + $baseReposURL + "/" + $fullName
 
-#$StargazerArray=Invoke-RestMethod -headers $Headers -uri "https://api.github.com/repos/$UserName/$RepoName/stargazers"
-#$StargazerArray[0]
+$repo=$(GetRepo).Repo
+$forkArray=$(GetForks).ForkArray
+$forkArray
+
+$action = "";
+$newRepoIndex = -1; 
+while ($action -ne "x"){
+	DisplayRepo
+	DisplayForkTable
+	
+	$action = Read-Host -Prompt "`nWhat action would you like to take? `nPossible Actions: `n`t(G)o to fork`n`te(X)it"
+	if ($action -eq "g"){
+		
+		$newRepoIndex = Read-Host -Prompt "Please select 0 to see Parent Repository or the index of the fork you would like to see"
+		if ($newRepoIndex -eq 0) {
+			$repoURL = $(GetParentUserRepoURL).URL
+			$repo=$(GetRepo).Repo
+			$forkArray=$(GetForks).ForkArray
+		}	
+		else {
+			$parentUserRepo.Set_Item($forkArray[$newRepoIndex].url,$repo.url)
+			$repoURL = $forkArray[$newRepoIndex].url
+			$repo=$(GetRepo).Repo
+			$forkArray=$(GetForks).ForkArray
+		}
+	}
+}
