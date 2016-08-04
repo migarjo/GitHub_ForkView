7713c0527015cb5a66fd8131bab83140f133cfca
Michael Johnson <mjohn174@gmail.com>
2016-08-03 23:10:40 -0500

    Create Display and Get methods
    I'm looking to make the act of getting and displaying repos and forks
    repeatable, since I'd like to provide the ability to navigate between
    the forks of a given repo. In the interest of that, I've refactored each
    Get and Display of these into reusable functions.
    
    Created a display method with some key info on the repo selected
    
    Separated "GetRepo" into its own method
    
    Separated Fork display for loop into its own method (DisplayForkTable),
    and organized it into a table rather than a list of descriptive
    sentences

--- GitHub_DataView.ps1
+++ GitHub_DataView.ps1
@@ -5,11 +5,16 @@ $RepoName='PokemonGo-Map'
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}" -f $Env:GITHUB_OAUTH_TOKEN)))
 $Headers=@{Authorization=("Basic {0}" -f $base64AuthInfo)}
 
+Function GetRepo{
+	$value = @{}
+	$value.Repo=Invoke-RestMethod -headers $Headers -uri "https://api.github.com/repos/$UserName/$RepoName"
 
-$Repo=Invoke-RestMethod -headers $Headers -uri "https://api.github.com/repos/$UserName/$RepoName"
-$Repo
+	return $value
+}
 
-
+Function DisplayRepo{
+	Write-Host "`n`n`nRepository: "$Repo.full_Name"`nDescription: "$Repo.description"`nStars: "$Repo.stargazers_count"`nForks: "$Repo.forks_count
+}
 
 
 
@@ -23,16 +28,23 @@ Function GetForks{
 
 	$value.ForkArray=Invoke-RestMethod -headers $Headers -uri $Repo.forks_url -Body $value.Parameters
 
-
-	
 	return $value
 }
 
-$ForkArray=$(GetForks).ForkArray
-
-foreach ($Fork in $ForkArray) {
-	Write-Host "User " $Fork.owner.login"'s fork has "$Fork.stargazers_count" stars and "$Fork.forks_count" forks."
+Function DisplayForkTable{
+	Write-Host "`n`nNumber`t`tUser`t`t`tStars`t`tForks`t`t"
+	
+	$ForkIncr = 1
+	for ($i=1;$i -lt 11;$i++) {
+		Write-Host "$i`t`t"$ForkArray[$i].owner.login"`t`t"$ForkArray[$i].stargazers_count"`t`t"$ForkArray[$i].forks_count
+	}
 }
+
+
+$Repo=$(GetRepo).Repo
+DisplayRepo
+$ForkArray=$(GetForks).ForkArray
+DisplayForkTable
 
 #$StargazerArray=Invoke-RestMethod -headers $Headers -uri "https://api.github.com/repos/$UserName/$RepoName/stargazers"
 #$StargazerArray[0]
